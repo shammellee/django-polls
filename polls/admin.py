945e4026bdb552a84e7c97056e98b9b6b5a39142
Shammel Lee <holler@shammellee.com>
2017-04-14 17:21:01 -0400

    Complete part 7

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-# Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 3  # Provide enough fields for 3 choices
+
+
+class QuestionAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields': ['question_text']})
+    ,('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
+  ]
+
+  # Allow Choice objects to be edited on the Question admin page
+  inlines = [ChoiceInline]
+
+  # Display question_text, pub_date, and was_published_recently fields on the
+  # change list page
+  list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+  list_filter = ['pub_date']  # Filter by pub_date
+  search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -3,8 +3,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
 class Question(models.Model):
   question_text = models.CharField(max_length=200)
   pub_date      = models.DateTimeField('date_published')
@@ -16,6 +14,16 @@ class Question(models.Model):
     now = timezone.now()
 
     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+  # Position after the pub_date field
+  was_published_recently.admin_order_field = 'pub_date'
+
+  # Show an icon instead of True/False strings
+  was_published_recently.boolean           = True
+
+  # Set the column header
+  was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
   question    = models.ForeignKey(Question,on_delete=models.CASCADE)
